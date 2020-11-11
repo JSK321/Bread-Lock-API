3298b61a1e446c7b57ba1ad3da9c8172401f66c0
David Guthmann <drguthmann@cox.net>
2020-11-11 15:07:22 -0600

    init foodbank model

---
+++ models/foodbank.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+    // Store Customer values for interaction with other models
+    const FoodBank = sequelize.define("FoodBank", {
+        bankName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // email force not null would be better, for login later
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // Everything else "can" be null, but most won't
+        phone: {
+            type: DataTypes.STRING
+        },
+        address: {
+            type: DataTypes.STRING
+        },
+        // Used cityName and stateName because state is used in react, so don't want to overwrite
+        cityName: {
+            type: DataTypes.STRING
+        },
+        stateAbr: {
+            type: DataTypes.STRING
+        }
+    })
+    FoodBank.associate = function(models) {
+        // add associations here
+        // Orders will only ever belong to a Customer or a FoodBank and if either are deleted the order should be
+        FoodBank.hasMany(models.Orders, {
+            onDelete: "cascade"
+        });
+    };
+    
+
+    return FoodBank;
+}
+
+// In the routes you'd need a Character.addClass(id of the class), for post request
