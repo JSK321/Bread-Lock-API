b70d503dae36d7a358206d25e7384e7989faa59f
David Guthmann <drguthmann@cox.net>
2020-11-11 12:35:12 -0600

    adding server boilerplate

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //dependencies 
 const express = require("express")
-const exphbs = require("express-handlebars")
+
 
 //sets up the express app
 var app = express();
@@ -11,21 +11,23 @@ app.use(express.json());
 
 app.use(express.static("public"))
 
-//set handlebars as default template
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 //require models for syncing
 var db = require("./models");
 
 //Import Routes for controllers
-const characterController = require('./controllers/character-controller')
-app.use(characterController)
 
-const htmlController = require('./controllers/html-controller')
-app.use(htmlController)
+// Rename Controllers to Reflect Bread-Lock
+
+// const characterController = require('./controllers/character-controller')
+// app.use(characterController)
+
+// const htmlController = require('./controllers/html-controller')
+// app.use(htmlController)
 
 //syncing our sequelize models and then starting express app
+
+// Remember force: true restarts the server AND will need RESEEDING
+
 db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
