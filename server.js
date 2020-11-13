fcfbd8b4561dde53c01874443e4c74b7f0e8894d
David Guthmann <drguthmann@cox.net>
2020-11-13 17:53:31 -0600

    Changed port front end also used 3000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 //sets up the express app
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
